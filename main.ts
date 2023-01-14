aaef598cd6ec69aeec20961e86e203ba1fa16a2b
aki-taylor <112258478+aki-taylor@users.noreply.github.com>
2023-01-14 15:17:56 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,33 @@ input.onButtonPressed(Button.A, function () {
     } else {
         basic.showArrow(ArrowNames.North)
     }
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
+    方角 = input.compassHeading()
+    if (方角 < 22) {
+        basic.showArrow(ArrowNames.North)
+    } else if (方角 < 67) {
+        basic.showArrow(ArrowNames.NorthWest)
+    } else if (方角 < 112) {
+        basic.showArrow(ArrowNames.West)
+    } else if (方角 < 157) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (方角 < 202) {
+        basic.showArrow(ArrowNames.South)
+    } else if (方角 < 247) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (方角 < 292) {
+        basic.showArrow(ArrowNames.East)
+    } else if (方角 < 337) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
+    basic.pause(500)
     basic.clearScreen()
 })
